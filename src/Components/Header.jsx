bd3415d2b3fab258e5dee12f35a490ef8389d1f3
Subhashinibalu <subhashiniulk@gmail.com>
2024-07-25 21:45:50 +0530

    Header added to each page

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,32 +1,28 @@
 import React from 'react';
 import { Navbar } from "flowbite-react";
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <>
-          <Navbar fluid rounded className='bg-blue-200  p-5 shadow-lg  m-4 rounded-full   ' data-aos="fade-in">
+        <div className='p-4'>
+          <Navbar fluid rounded className=' bg-customyellow p-5 shadow-lg  mx-4  rounded-full   ' data-aos="fade-in" data-aos-duration="1500"
+            data-aos-easing="linear">
         
           <Navbar.Brand href='#'>
      
-        <span className="self-center whitespace-nowrap text-xl font-semibold ms-10 " >SUBHASHINI</span>
+        <span className="self-center whitespace-nowrap text-xl font-semibold font-namefont  ms-10 " >Subhashini</span>
       </Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse className='me-10'>
-      <Navbar.Link  href="#">
-          Home
-        </Navbar.Link>
-        <Navbar.Link href="#about" >
-          About
-        </Navbar.Link>
-        <Navbar.Link  href="#skills">
-          Skills
-        </Navbar.Link>
-        <Navbar.Link href="#projects">Projects</Navbar.Link>
-       
+      <Navbar.Link  ><Link to='/'>Home</Link></Navbar.Link>
+        <Navbar.Link  ><Link to='/about'>About</Link></Navbar.Link>
+          
+        <Navbar.Link  ><Link to='/skills'>Skills</Link></Navbar.Link>
+        <Navbar.Link  ><Link to='/projects'>Projects</Link></Navbar.Link>
         <Navbar.Link href="#contact">Contact</Navbar.Link>
       </Navbar.Collapse>
     </Navbar>  
-        </>
+        </div>
     );
 };
 
